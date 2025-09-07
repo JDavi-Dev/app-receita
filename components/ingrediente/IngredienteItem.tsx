6eb1c4550e92430cdaa0875626e95e0a76ad82c8
JDavi-Dev <jdaviprofil@proton.me>
2025-09-07 18:07:47 -0300

    feat: Modelar entidade Ingrediente e criar o item

---
+++ components/ingrediente/IngredienteItem.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Ingrediente } from "@/interfaces/Ingrediente";
+
+interface IngredienteProps {
+  ingrediente: Ingrediente;
+}
+
+function IngredienteItem({ ingrediente }: IngredienteProps) {
+  return (
+    <View style={styles.ingredienteItem}>
+      <Text style={styles.ingredienteNome}>{ingrediente.nome}</Text>
+      <Text style={styles.camposIngrediente}>
+        Quantidade: {ingrediente.quantidade}
+      </Text>
+      <Text style={styles.camposIngrediente}>Unidade de medida: {ingrediente.unidadeMedida}</Text>
+      <Text style={styles.camposIngrediente}>Categoria: {ingrediente.categoria}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  ingredienteItem: {
+    width: 300,
+    padding: 10,
+    marginTop: 7,
+    marginBottom: 3,
+    borderRadius: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+    alignItems: "center",
+    backgroundColor: "#9a5ff7",
+  },
+  ingredienteNome: {
+    color: "#820847",
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  camposIngrediente: {
+    color: "white",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+});
+
+export default IngredienteItem;
---
+++ interfaces/Ingrediente.tsx
@@ -0,0 +1,7 @@
+export interface Ingrediente {
+    id: string;
+    nome: string;
+    quantidade: string;
+    unidadeMedida: string;
+    categoria: string;
+}
